a6a96699a53e75e3e425ebd6eb0c0c3c3859c87e
mbarone <magnobarone@outlook.com>
2019-11-28 00:08:42 -0300

    Estudo de Herança

--- oo/carro.py
+++ oo/carro.py
@@ -72,6 +72,29 @@
     'Oeste'
 """
 
+class Carro:
+    def __init__(self, direcao, motor):
+        self.motor = motor
+        self.direcao = direcao
+
+    def calcular_velocidade(self):
+        return self.motor.velocidade
+
+    def acelerar(self):
+        return self.motor.acelerar()
+
+    def frear(self):
+        return self.motor.frear()
+
+    def calcular_direcao(self):
+        return self.direcao.valor
+
+    def girar_a_direita(self):
+        self.direcao.girar_a_direita()
+
+    def girar_a_esquerda(self):
+        self.direcao.girar_a_esquerda()
+
 NORTE = 'Norte'
 SUL = 'Sul'
 LESTE = 'Leste'
